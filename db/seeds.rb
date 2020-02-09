a125752a2a7179c6d8313f16ed82b72b676be9d1
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-02-09 02:45:28 +0000

    Fixed the isuue regarding double-sign-out Admin & Member

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(
+   email: 'a@a',
+   password: 'aaaaaa'
+)
+
+
+[
+  ['1', '太郎', 'タロウ', '山田', 'ヤマダ', '123-1111', '大阪府大阪市中央区', 'a@a', 'aaaaaa', '090-1111-2222'],
+  ['1', '花子', 'ハナコ', '渡辺', 'ワタナベ', '123-2222', '大阪府大阪市阿倍野区', 'b@b', 'bbbbbb', '090-3333-4444'],
+  ['1', '徹郎', 'テツロウ', '田中', 'タナカ', '123-3333', '大阪府大阪市天王寺区', 'c@c', 'cccccc', '090-5555-6666'],
+  ['1', '徹子', 'テツコ', '鈴木', 'スズキ', '123-4444', '大阪府大阪市浪速区', 'd@d', 'dddddd', '090-7777-8888']
+].each do |a, b, c, d, e, f, g, h, i, j|
+  Member.create!(
+    [
+      {
+      member_status: a,
+      last_name: b,
+      last_name_kana: c,
+      first_name: d,
+      first_name_kana: e,
+      postal_code: f,
+      address: g,
+      email: h,
+      password: i,
+      phone_number: j
+      }
+    ]
+  )
+end
